828230fe7e0137576dbb6d87180719d93183e6b9
Crazyhead90 <20604229+Crazyhead90@users.noreply.github.com>
2018-11-04 13:06:41 +0100

    stats fix

--- cogs/stats.py
+++ cogs/stats.py
@@ -25,24 +25,25 @@ class Stats:
                 async with session.get("https://api.coingecko.com/api/v3/coins/helix", headers=headers) as response:
                     responseRaw = await response.read()
                     priceData = json.loads(responseRaw)
-                    for item in priceData:
-                        embed= discord.Embed(colour=0x00FF00)
+                    
+                    embed= discord.Embed(colour=0x00FF00)
                         embed.set_author(name='HLIX Information', icon_url="https://i.imgur.com/ZnwpZ3r.png")
-                        embed.add_field(name="Price (USD)", value="${}".format(item[tickers][0][converted_last]['usd']))
-                        embed.add_field(name="Price (BTC)", value="{} BTC".format(item[tickers][0][converted_last]['btc']))
+                        embed.add_field(name="Price (USD)", value="${}".format(priceData['tickers'][0]['converted_last']['usd']))
+                    embed.add_field(name="Price (BTC)", value="{} BTC".format(priceData['tickers'][0]['converted_last']['btc']))
                         embed.add_field(name='\u200b',value='\u200b')
-                        embed.add_field(name="Volume (USD)", value="${}".format(item[tickers][0]['volume']))
-                        embed.add_field(name="Market Cap", value="${}".format(item[market_data][market_cap]['usd']))
+                        embed.add_field(name="Volume (USD)", value="${}".format(priceData['tickers'][0]['volume']))
+                        embed.add_field(name="Market Cap", value="${}".format(priceData['market_data']['market_cap']['usd']))
                         embed.add_field(name='\u200b',value='\u200b')
-                        embed.add_field(name="% 24h", value="{}%".format(item[market_data]['price_change_percentage_24h']))
-                        embed.add_field(name="% 7d", value="{}%".format(item[market_data]['price_change_percentage_7d']))
-                        embed.add_field(name="Circulating Supply", value="{} HLIX".format(item[market_data]['circulating_supply']))
-                        embed.add_field(name="Total Supply", value="{} HLIX".format(item[market_data]['total_supply']))
+                        embed.add_field(name="% 24h", value="{}%".format(priceData['market_data']['price_change_percentage_24h']))
+                        embed.add_field(name="% 7d", value="{}%".format(priceData['market_data']['price_change_percentage_7d']))
+                        embed.add_field(name="Circulating Supply", value="{} HLIX".format(priceData['market_data']['circulating_supply']))
+                        embed.add_field(name="Total Supply", value="{} HLIX".format(priceData['market_data']['total_supply']))
                         embed.add_field(name="Maximum Supply", value="500,000,000 HLIX")
-                        embed.set_footer(text="", icon_url="https://i.imgur.com/ZnwpZ3r.png")
+                        embed.set_footer(text="https://www.coingecko.com/en/coins/helix", icon_url="https://i.imgur.com/ZnwpZ3r.png")
                     await self.bot.say(embed=embed)
-        except:
-            await self.bot.say(":warning: Error fetching prices!")
+        except Exception as e:
+		    await self.bot.say(str(e))
+#           await self.bot.say(":warning: Error fetching prices!")
 
 
 def setup(bot):
